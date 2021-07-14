c31e113d2a2496ca1bd413cc1b7a87e4a324b2a6
Rodrigo <84488991+roderiix@users.noreply.github.com>
2021-07-14 00:14:57 -0400

    actualizado 2

--- dashboard/AdminGestorNoticias.php
+++ dashboard/AdminGestorNoticias.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php
-    session_start();
-    $file = __FILE__;
-    include_once "../include/functions.php";
-    include_once "../config/config.php";
+session_start();
+$file = __FILE__;
+include_once "../include/functions.php";
+include_once "../config/config.php";
 ?>
 
 <head>
@@ -64,9 +64,12 @@
         <div class="container-center rounded">
             <section class="seccion">
                 <div class="container-Noticias">
+                    <div class="container-boton">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addcourse-modal">Añadir Noticias</button>
+                    </div>
                     <table class="table">
-                        <thead>
-                            <tr>
+                        <thead style="background-color: steelblue;">
+                            <tr style="height: 40px;">
                                 <th scope="col">Imagen</th>
                                 <th scope="col">Titulo</th>
                                 <th scope="col">Fecha de publicacion</th>
@@ -74,24 +77,30 @@
                                 <th scope="col">Accion</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="tbody">
                             <?php
                             $sql = "select * from noticias ORDER BY id DESC"; // mejorar query falta nombre del que subio la noticia
                             $resultado = mysqli_query($conexion, $sql);
                             while ($mostrar = mysqli_fetch_array($resultado)) {
                             ?>
-                            <tr>
-                                <td><img style="width: 150px; height: 150px; padding-left:25px;" src="data:image/jpg;base64,<?php echo base64_encode($mostrar["imagen"]); ?>"></td>
-                                <td><h4 class="card-title" style="text-align: center;"><?php echo $mostrar['titulo']; ?> </h4></td>
-                                <td><p class="card-text" style="text-align: center;"><small class="text-muted"><?php echo $mostrar['fecha']; ?></small></p></td>
-                                <td><p class="card-text" style="text-align: center;"><small class="text-muted"><?php echo "Autor: ",$mostrar['correo']; ?></small></p></td>
-                                <td>
-								    <div class="btn-group btn-group-sm" role="group">
-									    <a class="btn btn-secondary btn-sm" href="#">Modificar</a>
-									    <a class="btn btn-danger btn-sm" href="#">Eliminar</a>
-								    </div>
-								</td>                                
-                            </tr>
+                                <tr>
+                                    <td style="text-align: center;"><img style="width: 150px; height: 150px; padding-left:25px;" src="data:image/jpg;base64,<?php echo base64_encode($mostrar["imagen"]); ?>"></td>
+                                    <td>
+                                        <h4 class="card-title" style="text-align: center;"><?php echo $mostrar['titulo']; ?> </h4>
+                                    </td>
+                                    <td>
+                                        <p class="card-text" style="text-align: center;"><small class="text-muted"><?php echo $mostrar['fecha']; ?></small></p>
+                                    </td>
+                                    <td>
+                                        <p class="card-text" style="text-align: center;"><small class="text-muted"><?php echo "Autor: ", $mostrar['correo']; ?></small></p>
+                                    </td>
+                                    <td>
+                                        <div class="btn-group btn-group-sm" style="text-align: center;" role="group">
+                                            <a class="btn btn-secondary btn-sm" style="color:seagreen;" href="#">Modificar</a>
+                                            <a class="btn btn-danger btn-sm" style="color:red;" href="#">Eliminar</a>
+                                        </div>
+                                    </td>
+                                </tr>
                             <?php } ?>
                         </tbody>
                     </table>
