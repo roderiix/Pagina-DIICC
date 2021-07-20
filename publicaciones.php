6c139e46a603627c4db19e5619d82ae9cc420bc1
Omega2001Z <86634108+Omega2001Z@users.noreply.github.com>
2021-07-19 23:22:17 -0400

    Aplica Query en Publicaciones
    Se aplica el Query en Publicaciones haciendo que se llamen a los datos dentro de la base de datos

--- publicaciones.php
+++ publicaciones.php
@@ -36,187 +36,37 @@
     <div class="blog-area pt-150 pb-150">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="https://www.scopus.com/home.uri"><img src="img/blog/blog1.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Wilson Castillo / 2019 / Conference Proceeding / Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://www.scopus.com/home.uri">Desarrollo de un software de análisis de RNA con integración de mecanismos interactivos. </a></h2>
-                                <a href="https://www.scopus.com/home.uri">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="https://link.springer.com/article/10.1007%2Fs10921-019-0602-9"><img src="img/blog/blog2.png" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Vladimir Riffo / 2019 / Science Citation Index Expanded / WoS-Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://link.springer.com/article/10.1007%2Fs10921-019-0602-9">Handgun Detection in Single-Spectrum Multiple X-ray Views Based on 3D Object Recognition </a></h2>
-                                <a href="https://link.springer.com/article/10.1007%2Fs10921-019-0602-9">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="https://ieeexplore.ieee.org/document/8705157"><img src="img/blog/blog3.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Servando Campillay / 2019 / Conference Proceedings Citation Index / WoS-Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://ieeexplore.ieee.org/document/8705157">Portfolio of Soft Skills Monitoring </a></h2>
-                                <a href="https://ieeexplore.ieee.org/document/8705157">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="https://ieeexplore.ieee.org/document/8705259"><img src="img/blog/blog4.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Servando Campillay / 2019 / Conference Proceedings Citation Index / WoS-Scopus</p>
-                            </div>
-
-                            <div class="blog-bottom">
-                                <h2><a href="https://ieeexplore.ieee.org/document/8705259">Educational Software as a Support for Teaching in Programming.</a></h2>
-                                <a href="https://ieeexplore.ieee.org/document/8705259">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="https://www.scopus.com/home.uri"><img src="img/blog/blog10.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>John Wilmar Castro / 2018 / Conference Proceedings Citation Index / Scopus</p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://www.scopus.com/home.uri">Usability evaluation methods adopted in agile development processes.</a></h2>
-                                <a href="https://www.scopus.com/home.uri">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                   <?php
+                        $sql = "SELECT * FROM `publicaciones` ORDER BY id ASC"; // mejorar query falta nombre del que subio la noticia
+                        $resultado = mysqli_query($conexion, $sql);
+                        while ($mostrar = mysqli_fetch_array($resultado)){ ?>
 
 
-                <div class="col-md-4 col-sm-6 col-xs-12 ">
+                    <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="single-blog mb-60">
                         <div class="blog-img">
-                            <a href="https://linkinghub.elsevier.com/retrieve/pii/S0950584918302234"><img src="img/blog/blog5.jpg" alt="blog"></a>
+                             <?php echo '<a href="publicaciones.php?id=' . $mostrar["id"] . '"></a>'; ?><img src="data:image/jpg;base64,<?php echo base64_encode($mostrar["imagen"]); ?>"></a>
                             <div class="blog-hover">
                                 <i class="fa fa-link"></i>
                             </div>
                         </div>
                         <div class="blog-content">
                             <div class="blog-top">
-                                <p> John Wilmar Castro / 2019 / Science Citation Index Expanded / WoS-Scopus</p>
+                                <p><?php echo $mostrar['autor']; ?> / <?php echo $mostrar['fecha'];?> / <?php echo $mostrar['revision'];?></p>
                             </div>
                             <div class="blog-bottom">
-                                <h2><a href="https://linkinghub.elsevier.com/retrieve/pii/S0950584918302234s">Adapting usability techniques for application in open source Software: A multiple case study </a></h2>
-                                <a href="https://linkinghub.elsevier.com/retrieve/pii/S0950584918302234">Seguir leyendo</a>
+                                <h2><?php echo '<a href="' . $mostrar["acceso"] . '">' . $mostrar['titulo'] . '</a>'; ?></h2>
+                                <?php echo '<a href="' . $mostrar["acceso"] . '">Seguir Leyendo</a>'; ?>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="single-blog mb-60">
-                        <div class="blog-img">
-                            <a href="http://www.risti.xyz/index.php?option=com_content&view=article&id=32&Itemid=131&lang=es"><img src="img/blog/blog6.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>John Wilmar Castro / 2019 / Journal / Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="http://www.risti.xyz/index.php?option=com_content&view=article&id=32&Itemid=131&lang=es">Metrics that could be used in mobile applications development for people with visual impairments: A preliminary literature review </a></h2>
-                                <a href="http://www.risti.xyz/index.php?option=com_content&view=article&id=32&Itemid=131&lang=es">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 hidden-xs">
-                    <div class="single-blog">
-                        <div class="blog-img">
-                            <a href="https://link.springer.com/chapter/10.1007%2F978-3-030-02828-2_11"><img src="img/blog/blog7.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Héctor Cornide / Servando Campillay / 2019 / Conference Proceedings Citation Index / Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://link.springer.com/chapter/10.1007%2F978-3-030-02828-2_11">Identification of Skills for the Formation of Agile High Performance Teams: A Systematic Mapping </a></h2>
-                                <a href="https://link.springer.com/chapter/10.1007%2F978-3-030-02828-2_11">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 hidden-xs">
-                    <div class="single-blog">
-                        <div class="blog-img">
-                            <a href="https://ibima.org/accepted-paper/determinacion-de-zonas-climaticas-en-la-frontera-norte-de-chile-utilizando-y-evaluando-tecnicas-de-clustering/"><img src="img/blog/blog8.jpg" alt="blog"></a>
-                            <div class="blog-hover">
-                                <i class="fa fa-link"></i>
-                            </div>
-                        </div>
-                        <div class="blog-content">
-                            <div class="blog-top">
-                                <p>Wilson Castillo / 2018 / Conference Proceeding / Scopus </p>
-                            </div>
-                            <div class="blog-bottom">
-                                <h2><a href="https://ibima.org/accepted-paper/determinacion-de-zonas-climaticas-en-la-frontera-norte-de-chile-utilizando-y-evaluando-tecnicas-de-clustering/">Determinación de zonas climáticas en la frontera norte de Chile utilizando y evaluando técnicas de clustering </a></h2>
-                                <a href="https://ibima.org/accepted-paper/determinacion-de-zonas-climaticas-en-la-frontera-norte-de-chile-utilizando-y-evaluando-tecnicas-de-clustering/">Seguir leyendo</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              
+                                
 
-            </div>
+                    <?php  }?>
+               
+           
             <div class="row">
                 <div class="col-xs-12">
                     <div class="pagination">
